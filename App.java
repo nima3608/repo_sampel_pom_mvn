87a8c9c623e334eb21ab8328d312cc56df80896b
nfakhari-dev <nima.fakhari@materna.group>
2024-11-24 13:10:23 +0100

    change App.java

--- App.java
+++ App.java
@@ -1,7 +1,8 @@
 package com.example;
 
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
+        System.out.println("Hellochn, Maven und Nima   !");
     }
 }
